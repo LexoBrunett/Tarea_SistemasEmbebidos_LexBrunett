765280e01413998e80c60a8a8f251f8fb5be8cba
LexoBrunett <lbrunett@espol.edu.ec>
2021-06-02 23:43:23 -0500

    Final Part

--- TareaEmbebidos1_LexBrunett.c
+++ TareaEmbebidos1_LexBrunett.c
@@ -12,7 +12,11 @@ int main(){
     float gastos[cantgastos];
     float exceso[]= {11290,14390,17990,21600,43190,64770,86370,115140};//Array Estatico de la tabla de impuestos
     float Impuestos[]= {0,0.05,0.1,0.12,0.15,0.2,0.25,0.3,0.35};//Array de Impuestos a pagar segun la base imponible 
+    float tasabasica[]={0,0,155,515,948,4187,8503,13903,22534};
     int f;
+    float sumaventas;
+    float sumagastos;
+    float impuesto;
 
     printf("-----------------------------------------------------\n");
     printf("-           MINI CALCULADORA TRIBUTARIA             -\n");
@@ -52,6 +56,7 @@ int main(){
         printf("Por favor ingrese el valor del valor de la factura %i :",f);
         printf("\n");
         scanf("%.2f",&ventas[f]);
+        sumaventas = sumaventas + ventas[f];
     }
     printf("Por favor ingrese el número de Facturas de Gastos referentes a su razón Social: \n");
     scanf("%i",&cantgastos);
@@ -60,9 +65,28 @@ int main(){
         printf("Por favor ingresar el valor de la factura número %i :",f);
         printf("\n");
         scanf("%.2f",&gastos[f]);
+        sumagastos = sumagastos +gastos[f];
     }
+    //Una vez agregadas las facturas a sus vectores procederemos y sus sumas realizadas procedemos a obtener la Base imponible
+    float baseimpo = sumaventas - sumagastos;
+    baseimpo = fabs(baseimpo);//en caso de que haya perdida
+    //ahora recorreremos la tabla de impuestos para confimar que impuesto tendrá el usuario
+    int longitudexceso = sizeof(exceso) / sizeof(exceso[0]);
+    for(f=0;f<longitudexceso;f++)
+    {
+        if(baseimpo <= exceso[f])
+        {   
+            //Esta formula ayuda a solucionar los impuestos a pagar
+            impuesto = tasabasica[f] + ((baseimpo-exceso[f])*Impuestos[f]);
+        }
+        else if(baseimpo >=exceso[-1])
+        {
+            //Esta formula ayuda a solucionar los impuestos a pagar
+            impuesto = tasabasica[-1] + ((baseimpo-exceso[-1])*Impuestos[-1]);
+        }
 
-    printf("El valor a pagar de sus impuestos es de: \n");
+    }
+    printf("El valor a pagar de sus impuestos es de: %.2f",impuesto);
     system("pause");
     return 0;
 }
